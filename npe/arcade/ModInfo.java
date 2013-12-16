f91f0113823fc40c40e30995c3649d067b88e962
NPException <npexception42@gmail.com>
2013-12-16 10:17:06 +0100

    Time to increase the version number

--- npe/arcade/ModInfo.java
+++ npe/arcade/ModInfo.java
@@ -3,6 +3,6 @@ package npe.arcade;
 public class ModInfo {
     public static final String MOD_ID = "npearcade";
     public static final String NAME = "ArcadeMod";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.3";
     public static final String CHANNEL = "arcademod";
 }
