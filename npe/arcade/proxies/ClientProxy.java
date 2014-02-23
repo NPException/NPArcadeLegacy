316971b06f0f305ed68dbd23da2cf7904d45110c
NPException42 <npexception42@gmail.com>
2014-02-23 16:16:32 +0100

    Temporarily disabled tickhandler
    the TickHandler causes the difficulty to reset for some reason, so i
    will disable it for now.

--- npe/arcade/proxies/ClientProxy.java
+++ npe/arcade/proxies/ClientProxy.java
@@ -3,13 +3,10 @@ package npe.arcade.proxies;
 import npe.arcade.client.RenderArcadeBase;
 import npe.arcade.client.RenderArcadeStool;
 import npe.arcade.client.RenderArcadeTop;
-import npe.arcade.client.TickHandlerClient;
 import npe.arcade.tileentities.TileEntityArcade;
 import npe.arcade.tileentities.TileEntityArcadeBase;
 import npe.arcade.tileentities.TileEntityArcadeStool;
 import cpw.mods.fml.client.registry.ClientRegistry;
-import cpw.mods.fml.common.registry.TickRegistry;
-import cpw.mods.fml.relauncher.Side;
 
 public class ClientProxy extends CommonProxy {
 	@Override
@@ -22,7 +19,7 @@ public class ClientProxy extends CommonProxy {
 		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityArcade.class, new RenderArcadeTop());
 		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityArcadeBase.class, new RenderArcadeBase());
 		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityArcadeStool.class, new RenderArcadeStool());
-		TickRegistry.registerTickHandler(new TickHandlerClient(), Side.CLIENT);
+		//TickRegistry.registerTickHandler(new TickHandlerClient(), Side.CLIENT);
 		//MinecraftForgeClient.registerItemRenderer(BlockInfo.ARCADE_ID, new RenderArcadeTechne());
 	}
 }
