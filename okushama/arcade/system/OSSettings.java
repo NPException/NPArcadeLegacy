973a0cc585cb3546e139812036003f81468e35ca
NPException42 <npexception42@gmail.com>
2014-03-18 17:42:19 +0100

    Fixed some warnings

--- okushama/arcade/system/OSSettings.java
+++ okushama/arcade/system/OSSettings.java
@@ -1,6 +1,5 @@
 package okushama.arcade.system;
 
-import java.awt.Color;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -11,37 +10,40 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class OSSettings {
-	
+
 	private static final File config = new File("config/arcade-okushamaos.cfg");
 
-	public float[] colourForeground = {1f,1f,1f}, colourBackground = {0f,0f,0f};
-	
-	public void save(){
-		try{
+	public float[] colourForeground = { 1f, 1f, 1f }, colourBackground = { 0f, 0f, 0f };
+
+	public void save() {
+		try {
 			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(config)));
 			Gson gson = new GsonBuilder().setPrettyPrinting().create();
 			out.write(gson.toJson(this));
 			out.close();
-		}catch(Exception e){
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public static OSSettings load(){
+
+	public static OSSettings load() {
 		OSSettings settings = null;
-		try{
-			if(!config.exists()){
+		try {
+			if (!config.exists()) {
 				config.createNewFile();
 			}
 			FileReader fr = new FileReader(config);
 			settings = new Gson().fromJson(fr, OSSettings.class);
-			if(settings == null){
+			if (settings == null) {
 				settings = new OSSettings();
-			}else{
-				System.out.println("WOOT "+settings.colourBackground[2]);
+			}
+			else {
+				System.out.println("WOOT " + settings.colourBackground[2]);
 			}
 			settings.save();
-		}catch(Exception e){
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 		return settings;
